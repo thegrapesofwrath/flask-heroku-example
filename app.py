4e35126657594a2359e6695912e3941c1a67b093
Shawn Hartley <shawn-hartley@sbcglobal.net>
2021-06-07 19:49:45 -0500

    Added comments

--- app.py
+++ app.py
@@ -1,8 +1,16 @@
 #%%
 from flask import Flask, render_template
+# import myMachineLearningModel
 #%%
 app = Flask(__name__)
 #%%
 @app.route("/")
 def home():
     return render_template('index.html')
+
+#Route to a webpage with a form
+
+#Post route from the form (Asynchronously called by AJAX)
+# Call the machine learning model .predict method on the data that passed from the form
+# return Json and python dictionaries (Jsonify)
+# AJAX calling function on the webform will update a div id with the results
